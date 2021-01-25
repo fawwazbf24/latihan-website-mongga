ce415f165ccbb82b39637336fe55f7cb1d75e466
fawwazbf <fawwazbyru93@gmail.com>
2021-01-25 12:57:44 +0700

    tambah gambar dan css

--- register/views.py
+++ register/views.py
@@ -1,15 +1,10 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+# Create your views here.
 def index(request):
     context = {
         'titleID': 'WELCOME BACK',
         'titleEN': 'Login page',
-        'piclog': 'login.png',
-        'picg': 'Google.png',
-        'picl': 'LinkedIN.png',
-        'picf': 'Facebook.png',
-        'css': 'style.css',
         'navigasi': [
             ['/register', 'Register']
         ]
